9872eabbfd0f96a53ef8538e0ccdd850bbb9d063
Aakash J John <127650255+AakashJai06@users.noreply.github.com>
2025-10-28 19:02:01 +0530

    Refactor input/output messages and logic in F&F.c

--- Cycle3/First and Follow/F&F.c
+++ Cycle3/First and Follow/F&F.c
@@ -6,15 +6,20 @@ char prod[20][20], first[20][20], follow[20][20];
 int n;
 
 void add(char *res, char c) {
+    if (c == '\0') return;
     for (int i = 0; res[i]; i++) if (res[i] == c) return;
-    int len = strlen(res); res[len] = c; res[len + 1] = '\0';
+    int len = strlen(res);
+    res[len] = c;
+    res[len + 1] = '\0';
 }
 
 void findFirst(char *res, char c) {
     for (int i = 0; i < n; i++) {
         if (prod[i][0] == c) {
-            if (!isupper(prod[i][2])) add(res, prod[i][2]);
-            else findFirst(res, prod[i][2]);
+            int j = 3; // Skip A-> part
+            if (!prod[i][j]) continue;
+            if (!isupper(prod[i][j])) add(res, prod[i][j]);
+            else findFirst(res, prod[i][j]);
         }
     }
 }
@@ -22,7 +27,7 @@ void findFirst(char *res, char c) {
 void findFollow(char *res, char c) {
     if (c == prod[0][0]) add(res, '$');
     for (int i = 0; i < n; i++) {
-        for (int j = 2; prod[i][j]; j++) {
+        for (int j = 3; prod[i][j]; j++) {
             if (prod[i][j] == c) {
                 if (prod[i][j + 1] && !isupper(prod[i][j + 1]))
                     add(res, prod[i][j + 1]);
@@ -36,19 +41,23 @@ void findFollow(char *res, char c) {
 }
 
 int main() {
-    printf("Enter the number of productions: ");
+    printf("Enter number of productions: ");
     scanf("%d", &n);
-    printf("Enter productions:\n");
-    for (int i = 0; i < n; i++) scanf("%s", prod[i]);
+    printf("Enter productions (e.g. E->E+T):\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%s", prod[i]);
+        first[i][0] = '\0';
+        follow[i][0] = '\0';
+    }
 
     for (int i = 0; i < n; i++) findFirst(first[i], prod[i][0]);
     for (int i = 0; i < n; i++) findFollow(follow[i], prod[i][0]);
 
-    printf("\nFirsts:\n");
+    printf("\nFIRST sets:\n");
     for (int i = 0; i < n; i++)
-        printf("%c : %s\n", prod[i][0], first[i]);
+        printf("FIRST(%c) = { %s }\n", prod[i][0], first[i]);
 
-    printf("\nFollows:\n");
+    printf("\nFOLLOW sets:\n");
     for (int i = 0; i < n; i++)
-        printf("%c : %s\n", prod[i][0], follow[i]);
+        printf("FOLLOW(%c) = { %s }\n", prod[i][0], follow[i]);
 }
